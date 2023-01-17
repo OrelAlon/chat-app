b991430d0f9f961c1133c04e060ad6dc0632af30
OrelAlon <orela231089@gmail.com>
2023-01-17 10:15:56 +0200

    sending data to the codeblockPage

--- frontend/src/components/codeblockCard/CodeblockCard.jsx
+++ frontend/src/components/codeblockCard/CodeblockCard.jsx
@@ -1,11 +1,20 @@
+import { useHistory } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 import "./codeblockCard.css";
 
 const CodeblockCard = ({ codeblock }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/codeblock/${codeblock._id}`, { state: { codeblock } });
+  };
+
   return (
     <div className='codeblock-card'>
       {codeblock.title}
       <p> {codeblock.code}</p>
-      <button>code</button>
+      <button onClick={handleClick}>code</button>
     </div>
   );
 };
--- frontend/src/pages/codeblockPage/CodeblockPage.jsx
+++ frontend/src/pages/codeblockPage/CodeblockPage.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import { useLocation } from "react-router-dom";
 
 const CodeblockPage = () => {
+  const { codeblock } = useLocation().state;
+
+  if (!codeblock) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      CodeblockPage
-      <div> the code here</div>
+      <h1>{codeblock.title}</h1>
+      <pre>{codeblock.code}</pre>
     </div>
   );
 };
