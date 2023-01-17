5d3906194d3825e8158f2f9d6b935a6091475ec3
OrelAlon <orela231089@gmail.com>
2023-01-17 07:41:47 +0200

    add cors to backend

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,34 @@
 const express = require("express");
-const dotenv = require("dotenv").config();
+require("dotenv").config();
 const connect = require("./config/db");
+const cors = require("cors");
 
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+// middlewares
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://chat-app-rqlq.onrender.com/"],
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/codeblock", require("./codeblock/codeblock.routes"));
 
-// app.listen(port, () => console.log(`Server started on port ${port}`));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 
 app.listen(port, () => {
   connect(process.env.MONGO_URL);
