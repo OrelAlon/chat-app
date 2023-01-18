b76dc6a3ddb94beb663ae1d25aee4a7a6cd91943
OrelAlon <orela231089@gmail.com>
2023-01-18 11:58:04 +0200

    implement handleCheckCode

--- frontend/src/components/liveCodeblockCard/LiveCodeblockCard.jsx
+++ frontend/src/components/liveCodeblockCard/LiveCodeblockCard.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
 
 import io from "socket.io-client";
+import axios from "axios";
 
 const LiveCodeblockCard = ({ codeblock, codeblockId }) => {
   const [liveCode, setLiveCode] = useState(null);
   const [socket, setSocket] = useState(null);
+  const [isCorrect, setIsCorrect] = useState("");
 
   useEffect(() => {
     const s = io.connect("http://localhost:5000/");
@@ -39,13 +41,29 @@ const LiveCodeblockCard = ({ codeblock, codeblockId }) => {
     setLiveCode(newText);
     socket.emit("send-changes", { codeblockId, newText });
   };
+  const handleCheckCode = async (e) => {
+    e.preventDefault();
+    try {
+      const checkCode = await axios.get(`/api/codeblock/?id=${codeblockId}`);
+      if (
+        liveCode.replace(/\s/g, "") ==
+        checkCode.data.solution.replace(/\s/g, "")
+      ) {
+        setIsCorrect("🤗");
+      } else {
+        setIsCorrect("Try again...");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div>
       Student Page
       <h3>{codeblock.title}</h3>
       <pre>{codeblock.code}</pre>
-      <form action='/form/submit' method='GET'>
+      <form onSubmit={handleCheckCode}>
         <textarea
           rows='15'
           cols='60'
@@ -56,6 +74,7 @@ const LiveCodeblockCard = ({ codeblock, codeblockId }) => {
         <br />
         <input type='submit' value='submit' />
       </form>
+      <p>{isCorrect}</p>
     </div>
   );
 };
